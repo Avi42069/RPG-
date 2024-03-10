d69724435346d05fdbf004c39c75afe0d874074e
Avi42069 <129685082+Avi42069@users.noreply.github.com>
2024-03-10 17:13:45 +0530

    Add files via upload
    added ticktaktoe game

--- script.js
+++ script.js
@@ -1,86 +1,30 @@
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeapon = 0;
-let fighting;
-let monsterHealth;
-let inventory = ["stick"];
+let boxes= document.querySelectorAll(".button");
+let resetButton= document.querySelector(".rst_btn");
 
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth");
-const locations = [
-  {
-    name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goStore, goCave, fightDragon],
-    text: "You are in the town square. You see a sign that says \"Store\"."
-  },
-  {
-    name: "store",
-    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-    "button functions": [buyHealth, buyWeapon, goTown],
-    text: "You enter the store."
-  },
-  {
-    name: "cave",
-    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button functions": [fightSlime, fightBeast, goTown],
-    text: "You enter the cave. You see some monsters."
-  }
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
 ];
-
-// initialize buttons
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon;
-
-function update(location) {
-  button1.innerText = location["button text"][0];
-  button2.innerText = location["button text"][1];
-  button3.innerText = location["button text"][2];
-  button1.onclick = location["button functions"][0];
-  button2.onclick = location["button functions"][1];
-  button3.onclick = location["button functions"][2];
-  text.innerText = location.text;
+boxes.forEach((box) =>{
+     box.addEventListener("click", () =>
+{
+    let turn0=true;
+    if (turn0 === true)
+{
+        box.innerText="if";
+        let turnX=true;
 }
-
-function goTown() {
-  update(locations[0]);
+else{  
+    box.innerText="else";
 }
+    });
+});
 
-function goStore() {
-  update(locations[1]);
-}
 
-function goCave() {
-  update(locations[2]);
-}
-
-function fightDragon() {
-  console.log("Fighting dragon.");
-}
-
-function buyHealth() {
-  gold = gold - 10;
-  health = health + 10;
-}
-
-function buyWeapon() {
-
-}
-
-function fightSlime() {
-
-}
-
-function fightBeast() {
-
-}
